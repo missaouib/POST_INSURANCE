030d91f91c00a49a87be90d02a906047dd93db26
beming <beming@21cn.com>
2016-03-03 07:13:32 +0800

    improve hfgl

---
+++ post_insurance/src/java/com/gdpost/utils/TemplateHelper/CallFailDoorBackListColumn.java
@@ -0,0 +1,76 @@
+package com.gdpost.utils.TemplateHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CallFailDoorBackListColumn {
+
+	private static List<ColumnItem> standardColumns;
+	public static String KEY_ROW = "上交时间";
+	
+	public static List<ColumnItem> getStandardColumns() {
+		if(standardColumns != null) {
+			return(standardColumns);
+		}
+		
+		standardColumns = new ArrayList<ColumnItem>();
+		
+		ColumnItem column = new ColumnItem();
+		column.setDisplayName("保单号");
+		column.setColumnName("policy_no");
+		column.setColumnType(ColumnType.string);
+		column.setNullable(true);
+		column.setiOutputOrder(16);
+		column.setOutputName("QD");
+		standardColumns.add(column);
+		
+		/*
+		column = new ColumnItem();
+		column.setDisplayName("工单流水号");
+		column.setColumnName("issue_no");
+		column.setColumnType(ColumnType.string);
+		column.setNullable(false);
+		column.setNeedOutput(true);
+		column.setiOutputOrder(3);
+		column.setOutputName("GDLSH");
+		standardColumns.add(column);
+		
+		*/
+		column = new ColumnItem();
+		column.setDisplayName("上交时间");
+		column.setColumnName("door_back_date");
+		column.setColumnType(ColumnType.string);
+		column.setNullable(true);
+		standardColumns.add(column);
+		
+		column = new ColumnItem();
+		column.setDisplayName("统计时间");
+		column.setColumnName("door_stats_date");
+		column.setColumnType(ColumnType.string);
+		column.setNullable(true);
+		standardColumns.add(column);
+		
+		column = new ColumnItem();
+		column.setDisplayName("回访结果");
+		column.setColumnName("door_back_rst");
+		column.setColumnType(ColumnType.string);
+		column.setNullable(true);
+		standardColumns.add(column);
+		
+		column = new ColumnItem();
+		column.setDisplayName("回访时间");
+		column.setColumnName("door_date");
+		column.setColumnType(ColumnType.string);
+		column.setNullable(true);
+		standardColumns.add(column);
+		
+		column = new ColumnItem();
+		column.setDisplayName("回访人");
+		column.setColumnName("door_man");
+		column.setColumnType(ColumnType.string);
+		column.setNullable(true);
+		standardColumns.add(column);
+		
+		return(standardColumns);
+	}
+}
---
+++ post_insurance/src/java/com/gdpost/utils/TemplateHelper/CallFailMailBackListColumn.java
@@ -0,0 +1,69 @@
+package com.gdpost.utils.TemplateHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CallFailMailBackListColumn {
+
+	private static List<ColumnItem> standardColumns;
+	public static String KEY_ROW = "保单号";
+	
+	public static List<ColumnItem> getStandardColumns() {
+		if(standardColumns != null) {
+			return(standardColumns);
+		}
+		
+		standardColumns = new ArrayList<ColumnItem>();
+		
+		ColumnItem column = new ColumnItem();
+		column.setDisplayName("保单号");
+		column.setColumnName("policy_no");
+		column.setColumnType(ColumnType.string);
+		column.setNullable(true);
+		column.setiOutputOrder(16);
+		column.setOutputName("QD");
+		standardColumns.add(column);
+		
+		/*
+		column = new ColumnItem();
+		column.setDisplayName("工单流水号");
+		column.setColumnName("issue_no");
+		column.setColumnType(ColumnType.string);
+		column.setNullable(false);
+		column.setNeedOutput(true);
+		column.setiOutputOrder(3);
+		column.setOutputName("GDLSH");
+		standardColumns.add(column);
+		
+		*/
+		column = new ColumnItem();
+		column.setDisplayName("退信原因");
+		column.setColumnName("mail_fail_reason");
+		column.setColumnType(ColumnType.string);
+		column.setNullable(true);
+		standardColumns.add(column);
+		
+		column = new ColumnItem();
+		column.setDisplayName("退信时间");
+		column.setColumnName("mail_fail_date");
+		column.setColumnType(ColumnType.string);
+		column.setNullable(true);
+		standardColumns.add(column);
+		
+		column = new ColumnItem();
+		column.setDisplayName("信函成功");
+		column.setColumnName("mail_success");
+		column.setColumnType(ColumnType.string);
+		column.setNullable(true);
+		standardColumns.add(column);
+		
+		column = new ColumnItem();
+		column.setDisplayName("回邮时间");
+		column.setColumnName("mail_back_date");
+		column.setColumnType(ColumnType.string);
+		column.setNullable(true);
+		standardColumns.add(column);
+		
+		return(standardColumns);
+	}
+}
