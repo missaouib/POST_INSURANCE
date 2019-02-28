c907d1d618a123f3a756054fa06727208e931a71
beming <beming@21cn.com>
2019-02-28 17:20:49 +0800

    improve task: add renewed invalid to policy.

--- post_insurance/src/java/com/gdpost/web/service/task/TaskService.java
+++ post_insurance/src/java/com/gdpost/web/service/task/TaskService.java
@@ -166,8 +166,12 @@ public class TaskService {
 			statement.executeUpdate(sql);
 			log.info("------------ finish exec sql");
 			
-			sql = "update t_policy t0 set t0.status='有效' where t0.status<>'有效' and t0.policy_no in (select t2.policy_no from t_pay_success_list t1, t_cs_report t2 "
-					+ "where t1.pay_type=2 and t1.rel_no=t2.cs_no and t2.cs_code='RE');";
+			sql = "update t_policy tp, t_renewed_list rdl set tp.status=\"失效终止\" where tp.policy_no=rdl.policy_no and tp.status=\"有效\" and rdl.fee_status<>\"交费成功\" and TIMESTAMPDIFF(DAY,rdl.fee_date,now())>61 and rdl.policy_no not in (select psl.rel_no from t_pay_success_list psl where TIMESTAMPDIFF(DAY,psl.back_date,now())>60);";
+			log.info("------------ sql :" + sql);
+			statement.executeUpdate(sql);
+			log.info("------------ finish exec sql");
+			
+			sql = "update t_policy t0 set t0.status='有效' where t0.status<>'有效' and t0.policy_no in (select t2.policy_no from t_pay_success_list t1, t_cs_report t2 where t1.pay_type=2 and t1.rel_no=t2.cs_no and t2.cs_code='RE');";
 			log.info("------------ sql :" + sql);
 			statement.executeUpdate(sql);
 			log.info("------------ finish exec sql");
